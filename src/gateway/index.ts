255188355ac883ef36e336167f061517c7339057
David Alen <david.alen@fabric.inc>
2021-05-30 02:39:02 -0300

    (add) default

---
+++ src/gateway/index.ts
@@ -0,0 +1,23 @@
+import { ApolloServer } from 'apollo-server';
+import { ApolloGateway } from '@apollo/gateway';
+
+// Initialize an ApolloGateway instance and pass it an array of
+// your subgraph names and URLs
+const gateway = new ApolloGateway({
+    serviceList: [
+        { name: 'books', url: 'http://localhost:4001' },
+        // Define additional services here
+    ],
+});
+
+// Pass the ApolloGateway to the ApolloServer constructor
+const server = new ApolloServer({
+    gateway,
+
+    // Disable subscriptions (not currently supported with ApolloGateway)
+    subscriptions: false,
+});
+
+server.listen(3000).then(({ url }) => {
+    console.log(`🚀 Server ready at ${url}`);
+});
